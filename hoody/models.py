aa68867c4a03480f4bd3dd257b18d0f493c74eb0
go-max07 <johnmetry17@gmail.com>
2021-09-28 12:54:51 +0300

    Creation of 5 classes

--- hoody/models.py
+++ hoody/models.py
@@ -8,6 +8,9 @@ class Neighborhood(models.Model):
     location = models.CharField(max_length=60)
     occupantsCount = models.IntegerField(default=0, null=True, blank=True)
 
+    def __str__(self):
+        return self.name
+
 
 class User(models.Model):
     first_name = models.CharField(max_length=50)
