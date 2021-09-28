3ebdc2a5a1228b107132e75ec425044afa5bdf1c
go-max07 <johnmetry17@gmail.com>
2021-09-28 11:23:28 +0300

    Created five new models

--- hoody/models.py
+++ hoody/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+from django.urls import reverse
+
+
+class Neighborhood(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=60)
+    occupantsCount = models.IntegerField(default=0, null=True, blank=True)
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    neighborhood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE)
+    email = models.EmailField(max_length=70)
+
+
+class Business(models.Model):
+    name = models.CharField(max_length=150)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    neighborhood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE)
+    email = models.EmailField(max_length=70)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    categories = models.ManyToManyField('Category', related_name='posts')
+    neighborhood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    author = models.CharField(max_length=60)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
