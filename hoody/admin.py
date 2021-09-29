1546df1f1ff8659e642991bc90f738c0455eb737
go-max07 <johnmetry17@gmail.com>
2021-09-29 08:40:47 +0300

    Update of admin_list

--- hoody/admin.py
+++ hoody/admin.py
@@ -2,9 +2,18 @@ from django.contrib import admin
 
 from .models import *
 
+
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Post, PostAdmin),
+admin.site.register(Category, CategoryAdmin),
 admin.site.register(User),
 admin.site.register(Neighborhood),
 admin.site.register(Business),
-admin.site.register(Category),
-admin.site.register(Post),
 admin.site.register(Comment),
