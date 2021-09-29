0d414d8dded9959d159d642bdff82037ad5d5d4c
go-max07 <johnmetry17@gmail.com>
2021-09-29 08:42:08 +0300

    Updated form model

--- hoody/forms.py
+++ hoody/forms.py
@@ -36,3 +36,18 @@ class UserForm(forms.Form):
     last_name = forms.CharField()
     email = forms.EmailField()
 
+
+class CommentForm(forms.Form):
+    author = forms.CharField(
+        max_length=60,
+        widget=forms.TextInput(attrs={
+            "class": "form-control",
+            "placeholder": "Your Name"
+        })
+    )
+    body = forms.CharField(widget=forms.Textarea(
+        attrs={
+            "class": "form-control",
+            "placeholder": "Leave a comment!"
+        })
+    )
